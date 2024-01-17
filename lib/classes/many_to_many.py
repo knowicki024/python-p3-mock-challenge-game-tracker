ecb27cb3a088f51551724aa5f6faa9056bbbedd6
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-17 16:06:48 -0500

    B&B CORRECT CODE

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,39 +1,96 @@
-# import ipdb
+from statistics import mean
 
 class Game:
+    all = []
     def __init__(self, title):
         self.title = title
- 
+        type(self).all.append(self)
+        # print (f"type(self){type(self)}") 
+        
+    @property
+    def title(self):
+        return self._title
+        
+    @title.setter
+    def title(self, title):
+        if type(title) == str and len(title) > 0 and not hasattr(self, 'title'):
+            self._title = title 
+        # else:
+        #     raise Exception("Title must be a string and greater than 0")
+
+
     def results(self):
-        pass
+        return[result for result in Result.all if result.game == self]
 
     def players(self):
-        pass
+        return list({result.player for result in self.results()})
 
     def average_score(self, player):
-        pass
+        scores = [result.score for result in player.results() if result.game is self]
+        return mean(scores) if len(scores) else 0
 
 class Player:
+    all = []
     def __init__(self, username):
-        self.username = username
+        self._username = username
+        type(self).all.append(self)
+
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.player == self]
 
     def games_played(self):
-        pass
+        return list({result.game for result in self.results()})
 
     def played_game(self, game):
-        pass
+        return game in self.games_played() 
 
     def num_times_played(self, game):
-        pass
+        games_played = [result.game for result in self.results()]
+        return games_played.count(game)
 
 class Result:
+    all = []
     def __init__(self, player, game, score):
-        self.player = player
-        self.game = game
-        self.score = score
+        self._player = player
+        self._game = game
+        self._score = score
+        type(self).all.append(self)
+
+    @property
+    def player(self):
+        return self._player
+    
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+
+    @property 
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self._game = game 
+
+    @property 
+    def score(self):
+        return self._score 
+    
+    @score.setter 
+    def score(self, score):
+        if isinstance(score, int) and 1 <= score <= 5000 and not hasattr(self, 'score'):
+            self._score = score
 
 
-# ipdb.set_trace()
