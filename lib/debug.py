c7673de82547096921d4ed29d67730ad7bceb88a
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-18 18:47:12 -0500

    added tests to debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,17 @@ from classes.many_to_many import Result
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+
+    g1 = Game("Chess")
+    g2 =Game("Basketball")
+    g3 = Game("Monopoly")
+
+    p1 = Player("Melissa")
+    p2= Player("Hadil")
+    p3 = Player("Tyler")
+
+    r1 = Result(p1, g3, 100)
+    r2 = Result(p2, g1, 103)
+    r3 = Result(p3, g2, 110)
+
     ipdb.set_trace()
