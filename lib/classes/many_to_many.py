c970f0bdab7f04df3a4687895705e5df8c4f683c
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-18 19:21:21 -0500

    B&B

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,39 +1,97 @@
 # import ipdb
+from statistics import mean 
 
 class Game:
+    all = []
+
     def __init__(self, title):
-        self.title = title
+        self._title = title
+        Game.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title) > 0 and not hasattr(self, 'title'):
+            self._title = title
  
     def results(self):
-        pass
+        return [result for result in Result.all if result.game == self]
 
     def players(self):
-        pass
+        return list({result.player for result in self.results()})
 
     def average_score(self, player):
-        pass
+        return mean([result.score for result in player.results() if result.game is self])
 
 class Player:
+    all = []
+
     def __init__(self, username):
-        self.username = username
+        self._username = username
+        Player.all.append(self)
+
+    @property 
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.player == self]
 
     def games_played(self):
-        pass
+        return list({result.game for result in self.results()})
 
     def played_game(self, game):
-        pass
+        return game in self.games_played() 
 
     def num_times_played(self, game):
-        pass
+        games_played = [result.game for result in self.results()]
+        return games_played.count(game) 
 
 class Result:
+    all = []
+
     def __init__(self, player, game, score):
-        self.player = player
-        self.game = game
-        self.score = score
+        self._player = player
+        self._game = game
+        self._score = score
+        Result.all.append(self)
+    
+    @property
+    def score(self):
+        return self._score 
+    
+    @score.setter 
+    def score(self, score):
+        if isinstance(score, int) and 1 <= score <= 5000 and not hasattr(self, 'score'):
+            self._score = score 
+
+    @property
+    def player(self):
+        return self._player
+    
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player 
+
+    @property 
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self._game = game 
+
+
 
 
 # ipdb.set_trace()
